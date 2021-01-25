382297a23f03127391287444e492d83158590282
HarisRehan <73439246+HarisRehan@users.noreply.github.com>
2021-01-25 10:47:39 -0500

    Add files via upload

---
+++ p-34/Virtual-Pet--1-master/sketch.js
@@ -0,0 +1,28 @@
+//Create variables here
+var dog;
+var dogImg;
+var dogHappy;
+var database;
+
+function preload()
+{
+  //load images here
+  dogImg = loadImage ("images/dogImg.png");
+}
+
+function setup() {
+	createCanvas(800, 700);
+  dog = createSprite (250,250,40,40);
+   
+}
+
+
+function draw() {  
+
+  
+  //add styles here
+
+}
+
+
+
